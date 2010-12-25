a8096b98d58c85ce0bf63cfe9567d9e237afef5e
Michael de Silva <michael@mwdesilva.com>
2010-12-25 20:13:08 +0530

    troubleshooted compilation issues

--- src/com/threading/ThreadingActivity.java
+++ src/com/threading/ThreadingActivity.java
@@ -11,8 +11,8 @@ import android.widget.Button;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 
-public class ThreadingActivity extends Activity implements OnClickListener, Runnable {
-
+public class ThreadingActivity extends Activity implements OnClickListener {
+	Thread pipelineThread;
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -29,22 +29,23 @@ public class ThreadingActivity extends Activity implements OnClickListener, Runn
 
     public void onClick(View v) {
                           
-	    Thread pipelineThread = new Thread() {
+    	pipelineThread = new Thread() {
 	    	/*
-	    	 * 1. uncomment this and "Runnable" from above
-	    	 * 2. comment lines 68-80
+	    	 * 1. uncomment this and remove "Runnable" from above
+	    	 * 2. change new Thread(this) { to new Thread() above
+	    	 * 3. comment lines 68-80
 	    	 */
-//	        public void run(){
-//	        	final String TAG = "pipelineThread";
-//	                try {
-//	                	Log.i(TAG, "before sleep");
-//	                	Thread.sleep(1500); //just to show you that it works
-//	                	Log.i(TAG, "after sleep");
-//	                } catch (Exception e) {
-//	                        // @todo: Show error message
-//	                }
-//	                showResults.sendEmptyMessage(0);                                        
-//	        }
+	        public void run(){
+	        	final String TAG = "pipelineThread";
+	                try {
+	                	Log.i(TAG, "before sleep (" + this.getName() + ")");
+	                	Thread.sleep(1500); //just to show you that it works
+	                	Log.i(TAG, "after sleep (" + this.getName() + ")");
+	                } catch (Exception e) {
+	                        // @todo: Show error message
+	                }
+	                showResults.sendEmptyMessage(0);                                        
+	        }
 	    };
 	    pipelineThread.start();
     }
@@ -55,7 +56,7 @@ public class ThreadingActivity extends Activity implements OnClickListener, Runn
     	
     	@Override
     	public void handleMessage(Message msg) {
-    		Log.i(TAG, "received message: "+ (Object)msg.obj);
+    		Log.i(TAG, "received message: "+ (Object)msg.obj +  " (" + this.getLooper().getThread().getName() + ")");
     	
     		super.handleMessage(msg);
     	}
@@ -65,19 +66,19 @@ public class ThreadingActivity extends Activity implements OnClickListener, Runn
     /*
      * ThreadingActivity implements Runnable
      */
-	@Override
-	public void run() {
-		// TODO Auto-generated method stub
-    	final String TAG = "pipelineThread";
-        try {
-        	Log.i(TAG, "before sleep");
-        	Thread.sleep(1500); //just to show you that it works
-        	Log.i(TAG, "after sleep");
-        } catch (Exception e) {
-                // @todo: Show error message
-        }
-        showResults.sendEmptyMessage(0);  			
-	} 
+//    @Override
+//	public void run() {
+//		// TODO Auto-generated method stub
+//    	final String TAG = "pipelineThread";
+//        try {
+//        	Log.i(TAG, "before sleep (" + pipelineThread.getName() + ")");
+//        	Thread.sleep(1500); //just to show you that it works
+//        	Log.i(TAG, "after sleep (" + pipelineThread.getName() + ")");
+//        } catch (Exception e) {
+//            // @todo: Show error message
+//        }
+//    showResults.sendEmptyMessage(0);			
+//	} 
 		
 }
  
